568fcf6b33a895c5ad83c1f006587223c0fa6cb2
jirange <jirange@example.com>
2023-05-05 18:06:25 +0800

    update:实现火力道具 多线程

--- app/src/main/java/edu/hitsz/prop/BulletProp.java
+++ app/src/main/java/edu/hitsz/prop/BulletProp.java
@@ -1,10 +1,8 @@
 package edu.hitsz.prop;
 
 import edu.hitsz.aircraft.HeroAircraft;
-//import edu.hitsz.application.Main;
 import edu.hitsz.strategy.shoot.DirectShoot;
 import edu.hitsz.strategy.shoot.ScatteringShoot;
-//import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 
 
 /**
@@ -22,25 +20,20 @@ public class BulletProp extends BaseProp {
     public void activeProp(HeroAircraft heroAircraft) {
         System.out.println("FireSupply active!");
 //让直射线程暂停五秒 散射线程持续五秒后关闭
-//        Runnable scatteringRun = () -> {
-//            synchronized (BulletProp.class) {
-//                heroAircraft.setShootStrategy(new ScatteringShoot());
-//                try {
-//                    BulletProp.class.wait(5000);
-//                } catch (InterruptedException e) {
-//                    throw new RuntimeException(e);
-//                }
-//                heroAircraft.setShootStrategy(new DirectShoot());
-//            }
-//        };
-//        //todo 开启散射线程
-//        new Thread(scatteringRun).start();
-
-//        ScheduledExecutorService executorService= new ScheduledThreadPoolExecutor(1,
-//                new BasicThreadFactory.Builder().namingPattern("bullet-prop-action-%d").daemon(true).build());
-//
-//        executorService.schedule(scatteringRun,40, TimeUnit.MILLISECONDS);
-//        System.out.println("FireSupply end!");
+        Runnable scatteringRun = () -> {
+            synchronized (BulletProp.class) {
+                heroAircraft.setShootStrategy(new ScatteringShoot());
+                try {
+                    BulletProp.class.wait(5000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                heroAircraft.setShootStrategy(new DirectShoot());
+            }
+        };
+        //开启散射线程
+        new Thread(scatteringRun).start();
+        System.out.println("FireSupply end!");
 
     }
 
